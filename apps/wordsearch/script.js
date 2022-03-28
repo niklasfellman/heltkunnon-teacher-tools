b720fd5d89848c7b0bedcb789e05c60e0bb7d781
unknown <purjotti@gmail.com>
2022-03-28 19:55:23 +0300

    wordsearch update

--- apps/wordsearch/script.js
+++ apps/wordsearch/script.js
@@ -1,8 +1,9 @@
 const board = document.querySelector(".board")
 
 let gridSize = 10
-
 let boardArr = []
+
+let letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","t","u","v","x","y","z","å","ä","ö"]
 
 for (let i = 0; i < gridSize; i++) {
     boardArr.push([])
@@ -14,36 +15,42 @@ for (let i = 0; i < gridSize; i++) {
     }
 }
 
-let words = ["apa", "bur", "sol", "bak", "bit"]
+let words = ["apa", "bur", "sol","längre", "bak", "bit"]
 
-let placeWordsHelperDirection = function(p,number){
-    
-    if(number <.33) return [p[0] ++,p[1]]
-    else if(number <.66) return [p[0] ,p[1]++]
-    else return [p[0] ++,p[1]++]
-
+let placeWordsHelperDirection = function (p, number) {
+    if (number < .33) return [p[0]++, p[1]]
+    else if (number < .66) return [p[0], p[1]++]
+    else return [p[0]++, p[1]++]
 }
 
 let placeWords = function (w) {
 
-    for(let i = 0;i<words.length;i++){
-
-    let current = words[i]
-    console.log(current)
-
-    directionNumber = Math.random()
-
-    let pos = [Math.floor(Math.random() * boardArr.length), Math.floor(Math.random() * boardArr.length)]
-    while (pos[0] + current.length > boardArr.length || pos[1] + current.length > boardArr.length) {
-        console.log("new try")
-        pos = [Math.floor(Math.random() * boardArr.length), Math.floor(Math.random() * boardArr.length)]
+    for (let i = 0; i < words.length; i++) {
+        let current = words[i]
+        directionNumber = Math.random()
+        let pos = [Math.floor(Math.random() * boardArr.length), Math.floor(Math.random() * boardArr.length)]
+        while (pos[0] + current.length > boardArr.length || pos[1] + current.length > boardArr.length) {
+            pos = [Math.floor(Math.random() * boardArr.length), Math.floor(Math.random() * boardArr.length)]
+        }
+        for (let j = 0; j < current.length; j++) {
+            boardArr[pos[0]][pos[1]].innerText = current[j]
+            placeWordsHelperDirection(pos, directionNumber)
+        }
     }
-    for (let j = 0; j < current.length; j++) {
-        boardArr[pos[0]][pos[1]].innerText = current[j]
-        placeWordsHelperDirection(pos,directionNumber)
+}
+
+function fillWithLetters(){
+    for(let i = 0;i<boardArr.length;i++){
+        for(let j = 0;j<boardArr[i].length;j++){
+            let str = "" 
+            if(boardArr[i][j].innerText.length === 0) boardArr[i][j].innerText = letters[Math.floor(Math.random()*letters.length)] 
+        }
     }
-
-
-}}
+}
 
 placeWords()
+fillWithLetters()
+
+board.addEventListener("click",(e)=>{
+    console.log(e.target.innerText.length)
+})
