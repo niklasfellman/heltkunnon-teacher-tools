d07d3625e6001a02f27a5683396fb9968e1bae2d
unknown <purjotti@gmail.com>
2022-04-19 22:41:35 +0300

    adding languages

---
+++ script.js
@@ -0,0 +1,15 @@
+sessionStorage.setItem('lang', 'swe');
+
+console.log(sessionStorage.lang)
+
+const languageEl = document.querySelector(".language")
+
+console.log(languageEl)
+
+let test = document.createElement("li")
+
+test.setAttribute("W","w")
+
+console.log(test)
+
+languageEl.append(test)
