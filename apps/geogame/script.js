7cc72231f9e7663633d83d76d8623ed4acfd5178
unknown <purjotti@gmail.com>
2022-05-01 02:08:48 +0300

    geography update

--- apps/geogame/script.js
+++ apps/geogame/script.js
@@ -1,12 +1,13 @@
-let test = document.querySelectorAll("path")
+let paths = document.querySelectorAll("path")
 let boardElement = document.querySelector(".board")
 
 let countryToFindElement = document.querySelector(".country-to-find") 
 
 class Country{
-    constructor(dom){
+    constructor(dom,capital = "unknown"){
         this.domElement = dom 
         this.name= this.domElement.id;
+        this.capital = capital 
         this.color = `hsl(${(Math.floor(Math.random()*255))},70%,70%)`
     }
 
@@ -17,8 +18,8 @@ class Country{
 
 let countries = []
 let countriesToFind = []
-for(let i = 0;i<test.length;i++){
-    countries.push(new Country(test[i]))
+for(let i = 0;i<paths.length;i++){
+    countries.push(new Country(paths[i]))
     countriesToFind.push(countries[i].name)
     //console.log(countries[i].name)
 }
@@ -28,18 +29,16 @@ countryToFindElement.innerText = currentCountry
 
 boardElement.addEventListener("click", (e)=>{
     let clickedName = e.target.id
+    console.log(clickedName)
     let clickedCountry;
     for (let x of countries){
         if(x.name === clickedName){
          clickedCountry = x
         }
     }
-    console.log(clickedCountry.name)
-    console.log(currentCountry)
     if(clickedCountry.name === currentCountry){
         clickedCountry.changeColor()
         currentCountry = countriesToFind.splice(Math.floor(Math.random()*countriesToFind.length),1)[0]
-       console.log(countriesToFind)
         //currentCountry = countriesToFind.pop()
         countryToFindElement.innerText = currentCountry
     }
